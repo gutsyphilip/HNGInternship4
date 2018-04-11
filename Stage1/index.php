bcbf680f1209e06be71916328347dd2aad135075
Justine Philip <philip.obosi@eng.uniben.edu>
2018-04-11 10:54:42 +0100

    php and css complete

---
+++ Stage1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>HNG Internship Stage-1</title>
+</head>
+<body>
+    <main>
+    <div class="wrapper">
+    <div class="content">
+        <h1>Hi there, I'm Justine Philip</h1>
+        <h4>Fullstack Javascript Engineer.</h4>
+        <h6>Warm greetings from @worldclassdev</h6>
+        <p class="time"><?php echo date('D F j, g:i:s A'); ?></p>
+    </div>
+    </div>
+    </main>
+</body>
+</html>
